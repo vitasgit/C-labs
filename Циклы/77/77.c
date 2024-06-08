42544083efab306934dc4b91a0af78d66e7a249f
Vitaly <vibrantfeynman@gmail.com>
2024-06-08 11:32:43 +0500

    add 77.c

---
+++ Циклы/77/77.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) 
+{   
+    double res, s;
+    int i, j, n;
+
+    n = 3;
+
+    for (i = 1; i <= n; i++) {
+        s += sin((double)i);
+        res += 1 / s;
+
+    }
+
+    printf("%lf\n", res);
+
+    return 0;
+}
